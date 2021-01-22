a7b3a379ae7862f918ced4a3916fa77df23bf0fc
Matt Thorson <mattmakesgames@gmail.com>
2021-01-22 15:46:58 -0800

    Input stuff!

--- player.lua
+++ player.lua
@@ -10,12 +10,23 @@ end
 
 player.update = function(self) 
 	
+	local on_ground = self:check_solid(0, 1)
+
 	-- gravity
-	if (not self:check_solid(0, 1)) then
+	if (not on_ground) then
 		self.speed_y = min(self.speed_y + 10, 20)
 	end
 
+	-- running
+	self.speed_x += input_x
+
+	-- jumping
+	if (on_ground and input_jump_pressed) then
+		self.speed_y = -20
+	end
+
 	-- apply
+	self:move_x(self.speed_x)
 	self:move_y(self.speed_y)
 
 end
