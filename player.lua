f21e03934d9a4ae05089e677ff8616a48e689675
Noel Berry <thenberry@gmail.com>
2021-01-22 16:03:48 -0800

    scarf attempt

--- player.lua
+++ player.lua
@@ -43,6 +43,12 @@ end
 
 player.draw = function(self)
 	
+	local facing = self.right and 1 or -1
+	for i=0,3 do
+		local tx = self.x + 4 -facing * 4 - facing * i * 1
+		local ty = self.y + 5 + sin(i * 0.25 + time() * 2)
+		rect(tx, ty, tx, ty, 10)
+	end
 	-- draw sprite
 	object.draw(self)
 end
