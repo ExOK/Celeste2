25cfb01ac2be92a4e614c1a8a48aff9b9bacb9eb
Noel Berry <thenberry@gmail.com>
2021-01-23 16:05:01 -0800

    adding compress functions

---
+++ px9_decomp.lua
@@ -0,0 +1,114 @@
+-- px9 decompress
+-- by zep
+
+-- x0,y0 where to draw to
+-- src   compressed data address
+-- vget  read function (x,y)
+-- vset  write function (x,y,v)
+
+function
+    px9_decomp(x0,y0,src,vget,vset)
+
+    local function vlist_val(l, val)
+        -- find position
+        for i=1,#l do
+            if l[i]==val then
+                for j=i,2,-1 do
+                    l[j]=l[j-1]
+                end
+                l[1] = val
+                return i
+            end
+        end
+    end
+
+    -- bit cache is between 16 and 
+    -- 31 bits long with the next
+    -- bit always aligned to the
+    -- lsb of the fractional part
+    local cache,cache_bits=0,0
+    function getval(bits)
+        if cache_bits<16 then
+            -- cache next 16 bits
+            cache+=%src>>>16-cache_bits
+            cache_bits+=16
+            src+=2
+        end
+        -- clip out the bits we want
+        -- and shift to integer bits
+        local val=cache<<32-bits>>>16-bits
+        -- now shift those bits out
+        -- of the cache
+        cache=cache>>>bits
+        cache_bits-=bits
+        return val
+    end
+
+    -- get number plus n
+    function gnp(n)
+        local bits=0
+        repeat
+            bits+=1
+            local vv=getval(bits)
+            n+=vv
+        until vv<(1<<bits)-1
+        return n
+    end
+
+    -- header
+
+    local 
+        w,h_1,      -- w,h-1
+        eb,el,pr,
+        x,y,
+        splen,
+        predict
+        =
+        gnp"1",gnp"0",
+        gnp"1",{},{},
+        0,0,
+        0
+        --,nil
+
+    for i=1,gnp"1" do
+        add(el,getval(eb))
+    end
+    for y=y0,y0+h_1 do
+        for x=x0,x0+w-1 do
+            splen-=1
+
+            if(splen<1) then
+                splen,predict=gnp"1",not predict
+            end
+
+            local a=y>y0 and vget(x,y-1) or 0
+
+            -- create vlist if needed
+            local l=pr[a]
+            if not l then
+                l={}
+                for e in all(el) do
+                    add(l,e)
+                end
+                pr[a]=l
+            end
+
+            -- grab index from stream
+            -- iff predicted, always 1
+
+            local v=l[predict and 1 or gnp"2"]
+
+            -- update predictions
+            vlist_val(l, v)
+            vlist_val(el, v)
+
+            -- set
+            vset(x,y,v)
+
+            -- advance
+            x+=1
+            y+=x\w
+            x%=w
+        end
+    end
+end
